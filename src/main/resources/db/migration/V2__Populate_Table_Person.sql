95221a52c1614f4e19527421772f07eec90d2a7f
Raphael Andrade <raphael-andrade@live.com>
2022-11-24 11:35:11 -0300

    Implementação do Flyway migration

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+(1,'São Paulo','Airton','Male','Senna'),
+(2,'roma - Italy','Leonardo','Male','da Vinci'),
+(4,'marroco - US','Muhammad','Male','Ali'),
+(5,'Mvezo - Africa','Nelson','Male','Mandela'),
+(6,'Smiljan - Croacia','Nikola','Male','Tesla');
